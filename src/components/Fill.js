fd21e67fed067a85946ae244234ee7ff716d46ad
Grisel Matos <griselmatosm@gmail.com>
2020-04-01 17:57:35 +0200

    #3 / create fill component

---
+++ src/components/Fill.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../stylesheets/Fill.scss';
+
+const Fill = () => (
+<fieldset className="collapsible collapsable--open js-myform">
+  <div className="js-collapsible--trigger collapsible__header--fill">
+    <i className="far fa-keyboard collapsible__header--icon"></i>
+    <h2 className="collapsible__header__title">Rellena</h2>
+    <i className="fas fa-chevron-up collapsible__header--arrow-icon"></i>
+  </div>
+  <div className="collapsible__content">
+    <div>
+      <label className="collapsible__content--style-text required" htmlFor="fullname"> Nombre completo</label>
+      <input className="js-input-name js-input-text collapsible__content--input-form" placeholder="Ej: Sally Jill" type="text" id="fullname" name="name" required />
+    </div>
+    <div>
+      <label className="collapsible__content--style-text required" htmlFor="job"> Puesto</label>
+      <input className="js-input-job js-input-text collapsible__content--input-form" placeholder="Ej: Front-end" type="text" id="job" name="job" required />
+    </div>
+    <label className="collapsible__content--style-text" htmlFor="image"> Imagen de perfil</label>
+    <div className=" action collapsible__content--container-button">
+      <button className="action__upload-btn js__profile-trigger collapsible__content--style-text--button" type="button">Añadir imagen</button>
+      <input type="file" name="" id="img-selector" className="action__hiddenField js__profile-upload-btn" />
+    </div>
+    <div className="profile">
+      <div className="js__profile-preview collapsible__content--container-button--image-box"></div>
+    </div>
+    <label className="collapsible__content--style-text required" htmlFor="phone"> Teléfono</label>
+    <input className="js-input-phone js-input-text collapsible__content--input-form" placeholder="Ej: 555-55-55-55" type="tel" id="phone" name="phone" required />
+    <div>
+      <label className="collapsible__content--style-text required" htmlFor="email"> Email</label>
+      <input className="js-input-email  js-input-text collapsible__content--input-form" placeholder="Ej: sally-hill@gmail.com" type="email" id="email" name="email" required />
+    </div>
+    <div>
+      <label className="collapsible__content--style-text required" htmlFor="linkedin"> Linkedin</label>
+      <input className="js-input-linkedin js-input-text collapsible__content--input-form" placeholder="Ej: linkedin.com/in/sally.hill" type="url" id="linkedin" name="linkedin" required />
+    </div>
+    <div>
+      <label className="collapsible__content--style-text required" htmlFor="github"> Github</label>
+      <input className="js-input-github js-input-text collapsible__content--input-form" placeholder="Ej: @sally-hill" type="url" id="github" name="github" required />
+    </div>
+  </div>
+</fieldset>
+
+);
+
+export default Fill;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../stylesheets/Form.scss';
 import Design from './Design';
+import Fill from './Fill';
 
 const Form = () => (
   <form action="" method="POST" class="articles-container">
     <div class="principal-column">
       <Design />
-      <partial src="_forms-fill.html"></partial>
+      <Fill/>
       <partial src="_forms-share.html"></partial>
     </div>
   </form>
