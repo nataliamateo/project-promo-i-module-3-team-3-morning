538b2c0cc7ba659a7f6c71cc04ef3363865f941c
Grisel Matos <griselmatosm@gmail.com>
2020-04-01 17:40:09 +0200

    #3 / create design component

---
+++ src/components/Design.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../stylesheets/Design.scss';
+
+const Design = () => (
+<fieldset class="collapsible collapsable--open">
+  <div class="js-collapsible--trigger collapsible__header--design">
+    <i class="far fa-object-ungroup collapsible__header--icon"></i>
+    <h2 class="collapsible__header--title">Diseña</h2>
+    <i class="fas fa-chevron-up collapsible__header--arrow-icon"></i>
+  </div>
+  <div class="collapsible__content">
+    <h3 class="collapsible__content--title">Colores</h3>
+    <div class="collapsible__content--position">
+      <label class="js-palette1 collapsible__content__palette" for="firstcolor">
+        <input class="js-palette1 js-radio collapsible__content__palette--radio" id="firstcolor" type="radio" value="1" name="colors" checked="checked" />
+        <div class="collapsible__content__palette--box bg-dark-green"></div>
+        <div class="collapsible__content__palette--box bg-dirty-blue"></div>
+        <div class="collapsible__content__palette--box bg-hospital-green"></div>
+      </label>
+      <label class="js-palette2 collapsible__content__palette" for="secondcolor">
+        <input class="js-palette2 js-radio collapsible__content__palette--radio" id="secondcolor" type="radio" value="2" name="colors" />
+        <div class="collapsible__content__palette--box bg-dried-blood"></div>
+        <div class="collapsible__content__palette--box bg-rusty-red"></div>
+        <div class="collapsible__content__palette--box bg-tomato"></div>
+      </label>
+      <label class="js-palette3 collapsible__content__palette" for="tertiarycolor">
+        <input class="js-palette3 js-radio collapsible__content__palette--radio" id="tertiarycolor" type="radio" value="3" name="colors" />
+        <div class="collapsible__content__palette--box bg-slate"></div>
+        <div class="collapsible__content__palette--box bg-faded-orange"></div>
+        <div class="collapsible__content__palette--box bg-light-grey-blue"></div>
+      </label>
+    </div>
+  </div>
+</fieldset>
+);
+
+export default Design;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../stylesheets/Form.scss';
+import Design from './Design';
 
 const Form = () => (
-<form action="" method="POST" class="articles-container">
-  <div class="principal-column">
-    <partial src="_forms-design.html"></partial>
-    <partial src="_forms-fill.html"></partial>
-    <partial src="_forms-share.html"></partial>
-  </div>
-</form>
+  <form action="" method="POST" class="articles-container">
+    <div class="principal-column">
+      <Design />
+      <partial src="_forms-fill.html"></partial>
+      <partial src="_forms-share.html"></partial>
+    </div>
+  </form>
 );
 
 export default Form;
