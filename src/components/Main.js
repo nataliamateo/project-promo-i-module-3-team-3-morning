59dadf942ff1059e5f421c104fec85147e8a0c26
Natalia Mateo <nmateo7@gmail.com>
2020-04-03 11:12:51 +0200

    Fixed styles on main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import '../stylesheets/Main.scss';
-import Form from './Form';
-import Card from './Card';
+import React from "react";
+import "../stylesheets/Main.scss";
+import Form from "./Form";
+import Card from "./Card";
 
 const Main = () => (
   <section className="cards-page">
-<Form/>
-<Card/>
+    <Form />
+    <Card />
   </section>
 );
 
