ceb35b7f61e7c97aaa995dd0e6d744a6f00abf53
Natalia Mateo <nmateo7@gmail.com>
2020-04-03 12:35:31 +0200

    Create state object on main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,16 @@ import "../stylesheets/Main.scss";
 import Form from "./Form";
 import Card from "./Card";
 
+// this.state ={
+//     palette: selectedPalette,
+//     name: userName.value,
+//     job: userJob.value,
+//     mail: userMail.value,
+//     phone: userPhone.value,
+//     linkedin: userLinkedin.value,
+//     github: userGithub.value
+//    // }
+
 const Main = () => (
   <section className="cards-page">
     <Form />
