6509064d84fc743cd60b90f2cafb9b34e149bdb5
Natalia Mateo <nmateo7@gmail.com>
2020-04-04 17:37:30 +0200

    Move serviceWorker ubication

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import '../stylesheets/Form.scss';
-import Design from './Design';
-import Fill from './Fill';
-import Share from './Share';
+import React from "react";
+import "../stylesheets/Form.scss";
+import Design from "./Design";
+import Fill from "./Fill";
+import Share from "./Share";
 
-const Form = (props) => (
+const Form = props => (
   <form action="" method="POST" class="articles-container">
     <div class="principal-column">
-      <Collapsable>
-        <Design />
-      </Collapsable>
-      <Collapsable>
-        <Fill handleInput={props.handleInput} />
-      </Collapsable>
-      <Collapsable>
-        <Share />
-      </Collapsable>
+      {/* <Collapsable> */}
+      <Design />
+      {/* </Collapsable> */}
+      {/* <Collapsable> */}
+      <Fill handleInput={props.handleInput} />
+      {/* </Collapsable> */}
+      {/* <Collapsable> */}
+      <Share />
+      {/* // </Collapsable> */}
     </div>
   </form>
 );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.scss';
-import App from './components/App';
-import * as serviceWorker from './components/serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./stylesheets/index.scss";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
