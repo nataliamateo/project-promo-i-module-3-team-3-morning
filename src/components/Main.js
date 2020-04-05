3f48c68578453d5f86951326087bde1622ee3626
Natalia Mateo <nmateo7@gmail.com>
2020-04-05 17:18:11 +0200

    #4 Create userData object in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,30 +2,35 @@ import React from "react";
 import "../stylesheets/Main.scss";
 import Form from "./Form";
 import Card from "./Card";
- 
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userData: {
+        palette: "",
+        name: "",
+        job: "",
+        mail: "",
+        phone: "",
+        linkedin: "",
+        github: "",
+      },
+    };
   }
 
-  handleReset () {
+  handleReset() {
     // limpiará todos los valores
   }
 
   render() {
     const handleInput = (data) => {
       console.log(data);
-    }
+    };
     return (
       <section className="cards-page">
         <Form handleInput={handleInput} />
-        <Card
-          name={this.state.name}
-          job={this.state.job}
-          handleReset={this.handleReset}
-        />
+        <Card name={this.state.name} job={this.state.job} handleReset={this.handleReset} />
       </section>
     );
   }
