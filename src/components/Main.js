6179c986364e821be2b57333d30cfa42faf7d991
Natalia Mateo <nmateo7@gmail.com>
2020-04-06 14:17:58 +0200

    #4 Create object state on main

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,30 +7,42 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: {
-        palette: "",
-        name: "",
-        job: "",
-        mail: "",
-        phone: "",
-        linkedin: "",
-        github: "",
-      },
+      palette: 1,
+      name: "",
+      job: "",
+      email: "",
+      phone: "",
+      linkedin: "",
+      github: "",
     };
+    this.handleInput = this.handleInput.bind(this);
   }
 
   handleReset() {
     // limpiará todos los valores
   }
 
+  handleInput(data) {
+    debugger;
+    this.setState({
+      palette: data.name.value,
+      name: data.name ? data.name : data.value,
+      job: data.value,
+      email: data.value,
+      phone: data.value,
+      linkedin: data.value,
+      github: data.value,
+    });
+  }
+
   render() {
     const handleInput = (data) => {
-      console.log("desde main", data);
+      console.log(data);
     };
     return (
       <section className="cards-page">
         <Form handleInput={handleInput} />
-        <Card name={this.state.name} job={this.state.job} handleReset={this.handleReset} />
+        <Card palette={this.state.palette} name={this.state.name} job={this.state.job} handleReset={this.handleReset} handleInput={this.handleInput} />
       </section>
     );
   }
