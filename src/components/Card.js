c505ff75482acd93081415a08c9985350faf7f21
Margui <margui.berjon@gmail.com>
2020-04-10 01:20:38 +0200

    add props

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,29 +21,7 @@ const Card = (props) => (
             </h3>
           </div>
           <div className="profile__image js__profile-image"></div>
-          <Icons></Icons>
-          {/* <ul className="img-creation__card--list">
-            <li className="js-card-phone img-creation__card--round">
-              <a className="js-cardphone js-pastename" href={"tel:" + props.phone}>
-                <i className="fas fa-mobile-alt" title="Teléfono"></i>
-              </a>
-            </li>
-            <li className="js-card-email img-creation__card--round">
-              <a className="js-cardemail js-pastename" href={"mailto:" + props.email}>
-                <i className="far fa-envelope" title="Enviar email"></i>
-              </a>
-            </li>
-            <li className="js-card-linkedin img-creation__card--round">
-              <a className="js-cardlinkedin js-pastename" href={props.linkedin} alt="Linkedin">
-                <i className="fab fa-linkedin-in" title="Ir al Linkedin"></i>
-              </a>
-            </li>
-            <li className="js-card-github img-creation__card--round">
-              <a className="js-cardgithub js-pastename" href={props.github}>
-                <i className="fab fa-github-alt" title="Ir al Github"></i>
-              </a>
-            </li>
-          </ul> */}
+          <Icons phone={props.phone} email={props.email} linkedin={props.linkedin} github={props.github} />
         </article>
       </div>
     </div>
