5c6fc3bb441f1a65d0b8ecf968f069b583c7ebd9
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-11 17:36:50 +0200

    update placeholder for linkedin and github in fill component

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -10,7 +10,7 @@ const Fill = (props) => (
     <InputText id="job" labelText="Puesto" inputName="job" inputPlaceholder="Ej: Front-end" handleInput={props.handleInput} />
     <InputText id="phone" labelText="Teléfono" inputName="phone" inputType="tel" inputPlaceholder="Ej: 555-55-55-55" handleInput={props.handleInput} />
     <InputText id="email" labelText="Email" inputName="email" inputType="email" inputPlaceholder="Ej: sally-hill@gmail.com" handleInput={props.handleInput} />
-    <InputText id="linkedin" labelText="Linkedin" inputName="linkedin" inputType="linkedin" inputPlaceholder="Ej: linkedin.com/in/sally.hill" handleInput={props.handleInput} />
+    <InputText id="linkedin" labelText="Linkedin" inputName="linkedin" inputType="linkedin" inputPlaceholder="Ej: sally.hill" handleInput={props.handleInput} />
     {/* <label className="collapsible__content--style-text" htmlFor="image"> Imagen de perfil</label>
       <div className=" action collapsible__content--container-button">
         <button className="action__upload-btn js__profile-trigger collapsible__content--style-text--button" type="button">Añadir imagen</button>
@@ -19,7 +19,7 @@ const Fill = (props) => (
       <div className="profile">
         <div className="js__profile-preview collapsible__content--container-button--image-box"></div>
       </div> */}
-    <InputText id="github" labelText="Github" inputName="github" inputType="github" inputPlaceholder="Ej: @sally-hill" handleInput={props.handleInput} />
+    <InputText id="github" labelText="Github" inputName="github" inputType="github" inputPlaceholder="Ej: sally-hill" handleInput={props.handleInput} />
   </div>
 );
 
