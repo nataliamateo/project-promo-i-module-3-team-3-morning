e46c1586d4a377789792d5c4c26d89890f4a0dba
Grisel Matos <griselmatosm@gmail.com>
2020-04-20 20:12:44 +0200

    Add Home link in Header component using React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logoApp from '../images/descarga.svg';
 import '../stylesheets/Header.scss';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Header = () => (
   <header className="header__container">
     <img className="header__image" src={logoApp} title="Awesome profile-cards" alt="Awesome profile-cards" />
+    <Link className="header__home--container" title="Volver a Home" to='/'>
+      <i class="fas fa-home"></i>
+    </Link>
   </header>
 );
 
