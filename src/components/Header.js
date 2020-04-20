f92399fee72de89273a1f04433ca72e0ba189e23
Grisel Matos <griselmatosm@gmail.com>
2020-04-20 20:34:12 +0200

    Change className to class attribute

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
   <header className="header__container">
     <img className="header__image" src={logoApp} title="Awesome profile-cards" alt="Awesome profile-cards" />
     <Link className="header__home--container" title="Volver a Home" to='/'>
-      <i class="fas fa-home"></i>
+      <i className="fas fa-home"></i>
     </Link>
   </header>
 );
